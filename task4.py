4b83800e3e9d4e6c9c13531a9630a6622190f3e8
Andrey Ozhgibesov <wwwfox11@mail.ru>
2022-06-27 18:18:27 +0500

    Add task4.py

---
+++ task4.py
@@ -0,0 +1,26 @@
+# task4
+import itertools
+
+def bananas(s):
+    result = set()
+    
+    for comb in itertools.combinations(range(len(s)), len(s) - 6):
+        arr = list(s)
+        
+        for i in comb:
+            arr[i] = '-'
+        
+        candidate = ''.join(arr)
+        
+        if candidate.replace('-', '') == 'banana':
+            result.add(candidate)
+    
+    return result
+
+assert bananas("banann") == set()
+assert bananas("banana") == {"banana"}
+assert bananas("bbananana") == {"b-an--ana", "-banana--", "-b--anana", "b-a--nana", "-banan--a",
+                     "b-ana--na", "b---anana", "-bana--na", "-ba--nana", "b-anan--a",
+                     "-ban--ana", "b-anana--"}
+assert bananas("bananaaa") == {"banan-a-", "banana--", "banan--a"}
+assert bananas("bananana") == {"ban--ana", "ba--nana", "bana--na", "b--anana", "banana--", "banan--a"}
